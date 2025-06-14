11d9393f2e10983ebe2c63f1482e8964e685998f
Mentick3 <satishsahani234@gmail.com>
2025-06-14 21:44:52 +0530

    Update main.py.py

--- main.py.py
+++ main.py.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import requests
-import beautifulsoup4
+from bs4 import BeautifulSoup4
 import yfinance as yf
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
