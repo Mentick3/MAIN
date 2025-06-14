629a7670e1b470fd2af1d3ccc38486ee5c85a9af
Mentick3 <satishsahani234@gmail.com>
2025-06-14 21:57:39 +0530

    Update main.py.py

--- main.py.py
+++ main.py.py
@@ -1,7 +1,7 @@
-import pandas as pd
-import numpy as np
+import pandas
+import numpy 
 import requests
-import yfinance as yf
+import yfinance
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 import talib
